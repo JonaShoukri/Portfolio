45b82ddc4f90824fbda39e6b1025047731e3d99e
Jonas Shoukri <116384674+JonaShoukri@users.noreply.github.com>
2024-09-07 21:02:21 -0400

    postable

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import logo from "../assets/JSLogo.png";
 import {FaLinkedin} from "react-icons/fa";
 import {FaGithub} from "react-icons/fa";
 import { motion } from "framer-motion";
+import {ChakraProvider, FormControl, FormLabel, Switch} from '@chakra-ui/react'
 
 const Navbar = () => {
     return (
     <nav className="mb-20 flex item-center justify-between py-6">
         <div className="flex flexshrink-0 item-center">
-            <img className="mx-2 size-14" src={logo} alt="logo"/>
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
             <motion.a
